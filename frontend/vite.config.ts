36164d5960afa9562442e3e860222d8993f8004a
willy3087 <william.duarte@muralis.com.br>
2024-12-09 13:43:38 -0300

    Adiciona arquivos de configuração do ambiente para desenvolvimento, homologação e produção; atualiza caminhos de assets no index.html e configura opções de build no vite.config.ts.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["/assets/index-412d3335.js", "/assets/index-4d52487b.css"],
+    },
+  },
   server: {
     open: true,
   },
