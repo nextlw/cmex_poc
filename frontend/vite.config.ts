5cd7a6af7f5778287cbc9ffb5b10ea1fe25849f5
arthurvdcr <47759376+arthurvdcr@users.noreply.github.com>
2024-12-18 20:09:44 -0300

    Correções do vite.config.ts
    Estava configurado para buildar arquivos obsoletos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "./", // Garante caminhos relativos no HTML
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["/assets/index-412d3335.js", "/assets/index-4d52487b.css"],
+      // Remova a configuração `external`
+      output: {
+        manualChunks: undefined, // Para evitar divisão desnecessária de chunks
+      },
     },
   },
   server: {
-    open: true,
+    open: true, // Abre o navegador automaticamente
   },
 });
