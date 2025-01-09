faca6969197f292b5d030e5e98cf1bd2ac7d05d1
willy3087 <william.duarte@muralis.com.br>
2025-01-09 15:14:13 -0300

    refactor: reorganizar layout do componente BoxdeImpostos para melhor responsividade e clareza visual

--- frontend/src/components/BoxdeImpostos/index.tsx
+++ frontend/src/components/BoxdeImpostos/index.tsx
@@ -7,117 +7,96 @@ import { TbReceiptTax } from "react-icons/tb"; // Importa o ícone TbReceiptTax 
 const BoxdeImpostos: React.FC<BoxdeImpostosProps> = ({ classificacao }) => {
   return (
     <div className="info-basicas">
-      {" "}
-      {/* Define a div principal com a classe CSS "box-impostos" */}
-      <div className=" gap-4 w-full">
-        {" "}
-        <div className="flex items-center justify-between">
-          <h3 className="text-xl font-semibold text-white gap-2 flex items-center w-m-fit">
-            <TbReceiptTax /> Classificação Tributária
-          </h3>
-          <div className="flex space-x-16">
-            {" "}
-            {/* Define uma margem superior */}
-            <div className="flex space-x-4 w-full">
-              {" "}
-              {/* Define um layout flexível com espaçamento horizontal */}
-              <div className="flex items-center w-full">
-                {" "}
-                {/* Define um layout flexível centralizado verticalmente */}
-                <span className="box-resultado ml-2 text-gray-300 font-medium mr-2">
-                  Monofásico: {/* Texto "Monofásico:" */}
-                  <span
-                    className={`text-${classificacao.monofasico ? "green" : "red"
-                      }-500`}
-                  >
-                    {classificacao.monofasico ? "Sim" : "Não"}{" "}
-                    {/* Exibe "Sim" ou "Não" baseado no valor de monofasico */}
-                  </span>
-                </span>{" "}
-
-              </div>
-              <div className="flex items-center w-full min-w-fit">
-                {" "}
-                {/* Define um layout flexível centralizado verticalmente */}
-                <span className="box-resultado text-gray-300 font-medium  mr-2">
-                  Alíquota Zero: {/* Texto "Alíquota Zero:" */}
-                  <span
-                    className={`text-${classificacao.aliquota_zero ? "green" : "red"
-                      }-500`}
-                  >
-                    {classificacao.aliquota_zero ? "Sim" : "Não"}{" "}
-                    {/* Exibe "Sim" ou "Não" baseado no valor de aliquota_zero */}
-                  </span>
-                </span>{" "}
-              </div>
-            </div>
-          </div>
+      <div className="flex items-center w-full">
+        <h3 className="text-xl font-semibold text-white gap-2 flex items-center">
+          <TbReceiptTax /> Classificação Tributária
+        </h3>
+      </div>
+      <hr className="border-gray-600 my-4" />
+      <div className="grid grid-cols-2 w-full gap-3 mt-4">
+        <div className="flex w-full">
+          <span className="box-resultado text-gray-300 font-medium mr-2 flex w-full gap-1">
+            Monofásico:
+            <span
+              className={`text-${
+                classificacao.monofasico ? "green" : "red"
+              }-500 ml-1`}
+            >
+              {classificacao.monofasico ? "Sim" : "Não"}
+            </span>
+          </span>
         </div>
-        <hr className="border-gray-600 my-3" />
-        {/* Define um grid com 2 colunas e espaçamento de 4 */}
-        <div className="">
-          <h4 className="text-sm text-white ml-3 mb-2 mt-3">IPI</h4>{" "}
-          {/* Título da seção IPI */}
-          <div className="gap-3 grid grid-cols-2 m-2">
-            {" "}
-            {/* Define um espaço vertical entre os elementos */}
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Entrada: {classificacao.ipi_entrada} {/* Exibe o valor de ipi_entrada */}</span>{" "}
-
+        <div className="flex items-center">
+          <span className="box-resultado text-gray-300 font-medium mr-2 flex w-full gap-1">
+            Alíquota Zero:
+            <span
+              className={`text-${
+                classificacao.aliquota_zero ? "green" : "red"
+              }-500 ml-1`}
+            >
+              {classificacao.aliquota_zero
+                ? "Sim possui aliquota zero"
+                : "Não possui aliquota zero"}
+            </span>
+          </span>
+        </div>
+      </div>
+      <hr className="border-gray-600 my-4" />
+      <div className="grid grid-cols-2 gap-3 w-full">
+        <div>
+          <h4 className="text-sm text-white mb-2">IPI</h4>
+          <div className="flex w-full gap-3">
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Entrada: {classificacao.ipi_entrada}
+              </span>
             </p>
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Saída: {classificacao.ipi_saida} {/* Exibe o valor de ipi_saida */}</span>{" "}
-
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Saída: {classificacao.ipi_saida}
+              </span>
+            </p>
+          </div>
+          <h4 className="text-sm text-white mb-2 mt-4">COFINS</h4>
+          <div className="flex w-full gap-3">
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Entrada: {classificacao.cofins_entrada}
+              </span>
+            </p>
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Saída: {classificacao.cofins_saida}
+              </span>
             </p>
           </div>
         </div>
         <div>
-          <h4 className="text-sm text-white ml-3 mb-2 mt-3">PIS</h4>{" "}
-          {/* Título da seção PIS */}
-          <div className="gap-3 grid grid-cols-2 m-2">
-            {" "}
-            {/* Define um espaço vertical entre os elementos */}
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Entrada: {classificacao.pis_entrada} {/* Exibe o valor de pis_entrada */}</span>{" "}
-
+          <h4 className="text-sm text-white mb-2">PIS</h4>
+          <div className="flex w-full gap-3">
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Entrada: {classificacao.pis_entrada}
+              </span>
             </p>
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Saída: {classificacao.pis_saida} {/* Exibe o valor de pis_saida */}</span>{" "}
-
-            </p>
-          </div>
-        </div>
-        <div>
-          <h4 className="text-sm text-white ml-3 mb-2 mt-3">COFINS</h4>{" "}
-          {/* Título da seção COFINS */}
-          <div className="gap-3 grid grid-cols-2 m-2">
-            {" "}
-            {/* Define um espaço vertical entre os elementos */}
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Entrada: {classificacao.cofins_entrada}{" "}
-                {/* Exibe o valor de cofins_entrada */}</span>{" "}
-
-            </p>
-            <p className="text-gray-300">
-              <span className="box-resultado font-medium">Saída: {classificacao.cofins_saida} {/* Exibe o valor de cofins_saida */}</span>{" "}
-
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Saída: {classificacao.pis_saida}
+              </span>
             </p>
           </div>
-          <div>
-            <h4 className="text-sm text-white ml-3 mb-2 mt-3">CST</h4>{" "}
-            {/* Título da seção CST */}
-            <div className="gap-3 grid grid-cols-2 m-2">
-              {" "}
-              {/* Define um espaço vertical entre os elementos */}
-              <p className="text-gray-300">
-                <span className="box-resultado font-medium">Entrada: {classificacao.cst_entrada} {/* Exibe o valor de cst_entrada */}</span>{" "}
-
-              </p>
-              <p className="text-gray-300">
-                <span className="box-resultado font-medium">Saída: {classificacao.cst_saida} {/* Exibe o valor de cst_saida */}</span>{" "}
-
-              </p>
-            </div>
+          <h4 className="text-sm text-white mb-2 mt-4">CST</h4>
+          <div className="flex w-full gap-3">
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Entrada: {classificacao.cst_entrada}
+              </span>
+            </p>
+            <p className="w-1/2 text-gray-300">
+              <span className="box-resultado font-medium">
+                Saída: {classificacao.cst_saida}
+              </span>
+            </p>
           </div>
         </div>
       </div>
