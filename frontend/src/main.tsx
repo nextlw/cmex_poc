36fb172ff73e6153aea9ce41a7aa124275f777fb
willy3087 <william.duarte@muralis.com.br>
2025-01-13 09:31:54 -0300

    refactor: ajustar configuração do Uvicorn e melhorar estilos da interface

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,8 +11,8 @@ if (rootElement) {
   root.render(
     <React.StrictMode>
       <SessionProvider>
-        <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 p-8">
-          <div className="max-w-4xl mx-auto">
+        <div className="min-h-screen">
+          <div className="w-full mx-auto">
             <App />
           </div>
         </div>
