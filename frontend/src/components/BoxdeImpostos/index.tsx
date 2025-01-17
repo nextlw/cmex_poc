3a9a112bee108b098f8c2720a478eeb5b649fc54
willy3087 <william.duarte@muralis.com.br>
2025-01-17 10:55:45 -0300

    feat: update BoxdeImpostos component to enhance data display and styling
    - Replaced InputField components with static text displays for Monofásico, IPI, COFINS, PIS, and CST, improving clarity and user experience.
    - Enhanced visual representation by using colored text for Monofásico and Alíquota Zero statuses.
    - Streamlined the layout for better responsiveness and consistency across the component.
    - Updated historico_consultas.csv with new entries for better data representation.

--- frontend/src/components/BoxdeImpostos/index.tsx
+++ frontend/src/components/BoxdeImpostos/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { BoxdeImpostosProps } from "./types";
 import "./styles.css";
 import { PiSealPercentBold } from "react-icons/pi";
-import InputField from "../InputField";
 
 const BoxdeImpostos: React.FC<BoxdeImpostosProps> = ({ classificacao }) => {
   return (
     <div className="class-tributaria-title">
       <div className="flex items-center w-full">
         <h3 className="text-xl font-semibold color-gray-100 gap-2 flex items-center">
-          <PiSealPercentBold /> Classificação Tributária
+        <PiSealPercentBold /> Classificação Tributária
         </h3>
       </div>
       <hr className="border-gray-600 my-4" />
@@ -17,109 +16,91 @@ const BoxdeImpostos: React.FC<BoxdeImpostosProps> = ({ classificacao }) => {
         <div className="box-tributaria col-span-12 tablet-col-span-12 mobile-col-span-4">
           <div className="box-tributaria-row flex flex-row gap-4 md:flex-row mobile:flex-col">
             <div className="box-tributaria-item flex flex-col w-1/2 mobile:w-full gap-4">
-              <InputField
-                value={classificacao.monofasico ? "Sim" : "Não"}
-                onChange={() => {}}
-                readOnly={true}
-                label="Monofásico:"
-                showInnerLabel={true}
-              />
+              <span className="box-tributaria-item box-resultado text-gray-300 font-medium flex w-full gap-1">
+                Monofásico:
+                <span
+                  className={`text-${
+                    classificacao.monofasico ? "green" : "red"
+                  }-500 ml-1`}
+                >
+                  {classificacao.monofasico ? "Sim" : "Não"}
+                </span>
+              </span>
 
               <div className="box-tributaria-row">
                 <h4 className="text-sm mb-2">IPI</h4>
                 <div className="flex w-full gap-3">
-                  <InputField
-                    value={classificacao.ipi_entrada}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Entrada:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
-                  <InputField
-                    value={classificacao.ipi_saida}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Saída:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Entrada: {classificacao.ipi_entrada}
+                    </span>
+                  </p>
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Saída: {classificacao.ipi_saida}
+                    </span>
+                  </p>
                 </div>
               </div>
 
               <div className="box-tributaria-item">
                 <h4 className="text-sm mb-2">COFINS</h4>
                 <div className="flex w-full gap-3">
-                  <InputField
-                    value={classificacao.cofins_entrada}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Entrada:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
-                  <InputField
-                    value={classificacao.cofins_saida}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Saída:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Entrada: {classificacao.cofins_entrada}
+                    </span>
+                  </p>
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Saída: {classificacao.cofins_saida}
+                    </span>
+                  </p>
                 </div>
               </div>
             </div>
 
             <div className="box-tributaria-item flex flex-col w-1/2 mobile:w-full gap-4">
-              <InputField
-                value={classificacao.aliquota_zero ? "Sim" : "Não"}
-                onChange={() => {}}
-                readOnly={true}
-                label="Alíquota Zero:"
-                showInnerLabel={true}
-              />
+              <span className="box-resultado text-gray-300 font-medium flex w-full gap-1">
+                Alíquota Zero:
+                <span
+                  className={`text-${
+                    classificacao.aliquota_zero ? "green" : "red"
+                  }-500 ml-1`}
+                >
+                  {classificacao.aliquota_zero ? "Sim" : "Não"}
+                </span>
+              </span>
 
               <div className="box-tributaria-row">
                 <h4 className="text-sm mb-2">PIS</h4>
                 <div className="flex w-full gap-3">
-                  <InputField
-                    value={classificacao.pis_entrada}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Entrada:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
-                  <InputField
-                    value={classificacao.pis_saida}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Saída:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Entrada: {classificacao.pis_entrada}
+                    </span>
+                  </p>
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Saída: {classificacao.pis_saida}
+                    </span>
+                  </p>
                 </div>
               </div>
 
-              <div className="box-tributaria-item">
+              <div className="box-tributaria-item" >
                 <h4 className="text-sm mb-2">CST</h4>
                 <div className="flex w-full gap-3">
-                  <InputField
-                    value={classificacao.cst_entrada}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Entrada:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
-                  <InputField
-                    value={classificacao.cst_saida}
-                    onChange={() => {}}
-                    readOnly={true}
-                    label="Saída:"
-                    showInnerLabel={true}
-                    width="50%"
-                  />
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Entrada: {classificacao.cst_entrada}
+                    </span>
+                  </p>
+                  <p className="w-1/2 text-gray-300">
+                    <span className="box-resultado font-medium">
+                      Saída: {classificacao.cst_saida}
+                    </span>
+                  </p>
                 </div>
               </div>
             </div>
