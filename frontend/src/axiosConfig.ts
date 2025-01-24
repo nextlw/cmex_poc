9f16742dbaadb95962c36a2810103cdac9a91ffc
arthurvdcr <47759376+arthurvdcr@users.noreply.github.com>
2025-01-24 19:36:04 -0300

    fix: correção do axios

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -6,9 +6,7 @@ const ENV = import.meta.env.VITE_APP_NODE_ENV!
 // Define a URL base para as requisições do Axios
 const baseURL = ENV === "dev" 
   ? "http://localhost:10000/api"
-  : ENV === "homolog"
-    ? "https://cmex-poc-1.onrender.com/api"
-    : "https://cmex-poc.onrender.com/api"
+  : "https://cmex-poc.onrender.com/api"
 
 // Cria uma instância do Axios
 const axiosInstance = axios.create({
