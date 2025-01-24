e3ca9701263d8cb736c88e888b1d1f09985d6fd6
willy3087 <will.criando+nexcode-admin@gmail.com>
2025-01-24 08:07:43 -0300

    feat: update axios configuration and improve token handling
    - Adjusted the Axios instance configuration to include a timeout of 120 seconds for requests.
    - Simplified the retrieval of the Supabase token from localStorage by using a fixed project ID key.
    - Enhanced the token parsing logic to ensure proper extraction of the access token for authorization headers.

--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -6,25 +6,23 @@ const ENV = import.meta.env.VITE_APP_NODE_ENV!
 // Define a URL base para as requisições do Axios
 const baseURL = ENV === "dev"
   ? "http://localhost:10000/api"  
-  : "https://cmex-poc.onrender.com/api"  
+  : "https://cmex-poc.onrender.com/api"
 
 // Cria uma instância do Axios
 const axiosInstance = axios.create({
   baseURL,
+  timeout: 120000
 })
 
 // Intercepta as requisições
 axiosInstance.interceptors.request.use((config) => {
-
-  // ID do projeto no Supabase
-  const SUPABASE_PROJECT = import.meta.env.VITE_APP_SUPABASE_PROJECT!
-
-  // Token salvo no LocalStorage
-  const SUPABASE_TOKEN = localStorage.getItem(`sb-${SUPABASE_PROJECT}-auth-token`)
+  // Chave do localStorage com o ID correto do projeto
+  const SUPABASE_TOKEN = localStorage.getItem('sb-diyaxufhlpcxbevpbjsh-auth-token')
 
   // Adiciona o token no header dos chamados
   if (SUPABASE_TOKEN) {
-    config.headers.Authorization = `Bearer ${JSON.parse(SUPABASE_TOKEN).access_token}`
+    const parsedToken = JSON.parse(SUPABASE_TOKEN);
+    config.headers.Authorization = `Bearer ${parsedToken.access_token}`
   }
   
   // Define o tipo de dados passado nas requisições
