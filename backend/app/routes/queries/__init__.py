45888abeeb308681b95e33667d030249fc33ebfd
arthurvdcr <47759376+arthurvdcr@users.noreply.github.com>
2025-02-01 22:07:00 -0300

    fix: preencher campo "autocomplete" ao criar um registro na tabela de pesquisas

--- backend/app/routes/queries/__init__.py
+++ backend/app/routes/queries/__init__.py
@@ -97,6 +97,7 @@ async def post_queries(consulta_produto: ConsultaProduto, request: Request):
         consulta=consulta_produto.consulta,
         resultado=sugestao_ncm,  # TODO:
         duracao_da_query=elapsed_time,
+        autocomplete=False
     ).model_dump()
 
     # Salva a consulta na DB de pesquisas
